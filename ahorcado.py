c230136007e320af0b230a1c8949c169b865a9a8
Bladimir Escobar <bladimirescobardiaz@gmail.com>
2022-08-28 18:10:53 -0500

    mostrar el estado actual de la palabra

--- ahorcado.py
+++ ahorcado.py
@@ -1,9 +1,39 @@
 import random
+import string
 
 from list_palabras import palabras
 
 def obtener_palabra_valida(lista_palabras):
     palabra = random.choice(lista_palabras)
-    return palabra
 
-print(obtener_palabra_valida())
+    while "_" in palabra or " " in palabra:
+        palabra = random.choice(palabras)
+
+    return palabra.upper()
+
+
+def ahorcado():
+    print("""
+     +====================================+
+     | ¡Bienvenido al juego del ahorcado! |
+     +====================================+ 
+     """
+    )
+    palabra = obtener_palabra_valida(list_palabras)
+
+
+    letras_por_adivinar = set(palabra)
+    letras_adivinadas = set()
+    abecedario = set(string.ascii_uppercase)
+
+    vidas = 7
+
+    while len(letras_por_adivinar) > 0 and vidas > 0:
+        print(f"Te quedan {vidas} vidas y has usado estas letras:
+              {' '.join(letras_por_adivinar)}")
+
+        #Mostrar el estado actual de la palabra
+        palabra_lista = [letra if letra in letras_adivinadas
+        else '_' for letra in palabra]
+
+    
